ac346c11d0d4f942567576480ebed51a29e13ceb
Eric <ericaquino2002@gmail.com>
2024-06-05 22:30:41 -0300

    se aniade validaciones a paginas recuperarcontrasenia y principio de registrarusuario

--- scripts/validaciones.js
+++ scripts/validaciones.js
@@ -1,5 +1,5 @@
 function getPaginaActual(){
-    const path = window.location.pathname;
+    const path = document.location.pathname;
     const pagina = path.split("/").pop();
     return pagina;
 }
@@ -35,11 +35,75 @@ function paginaLogin(){
     }
 }
 
+function paginaRecuperarContraseña(){
+    window.onload = function(){
+        botonEnviar = document.getElementById("enviar");
+        botonEnviar.onclick = validarCampos;
+    }
+
+    function validarCampos(){
+        let formulario = document.getElementById("formulario");
+        let email = document.getElementById("email");
+        let usuario = document.getElementById("nombre_usuario");
+        let formularioCorrecto = true;
+
+        let validarEmail = /^(.+\@.+\..+)$/;
+
+        if(!validarEmail.test(email.value) || !email.value){
+            alert("Error en email, intenta de nuevo.")
+            formularioCorrecto = false;
+        }
+
+        if(!usuario.value){
+            alert("Pone tu usuario.")
+            formularioCorrecto = false;
+        }
+
+
+        if(formularioCorrecto){
+            formulario.submit();
+        }
+    }
+}
+
+
+function paginaRegistroUsuario(){
+    window.onload = function(){
+        var enviar = document.getElementById("enviar");
+        enviar.onclick = validarCampos;
+    }
+
+    function validarCampos(){
+        let formulario = document.getElementById("formulario");
+        let nombre = document.getElementById("nombre");
+        let apellido = document.getElementById("apellido");
+        let email = document.getElementById("email");
+
+        let validarNombre = /^[A-Za-z]+$/;
+
+        if(!validarNombre.test(nombre.value) || !nombre.value){
+            alert("Solo se admiten letras en tu nombre o esta vacio.")
+        }
+
+        let validarApellido = /^[A-Za-z]+$/;
+        if(!validarApellido.test(apellido.value) || !apellido.value){
+            alert("Solo se admiten letras en tu apellido o esta vacio.")
+        }
+    }
+}
+
 const paginaActual = getPaginaActual();
 
 if(paginaActual == "login.html"){
     paginaLogin();
 }
+if(paginaActual == "recuperar_contrasenia.html"){
+    paginaRecuperarContraseña();
+}
+if(paginaActual == "registro_usuario.html"){
+    paginaRegistroUsuario();
+}
+
 
 
 
